b0c4fe2f3ff855ca67c233673fba249eca33bcbc
sirse <hi@sirse.dev>
2025-12-28 21:40:55 +0200

    updates

--- crates/univm-risc0/src/compiler.rs
+++ crates/univm-risc0/src/compiler.rs
@@ -26,29 +26,36 @@ impl Compiler for Risc0Compiler {
 
         let elf_path = &entry.path;
 
-        write!(
-            generated_code,
-            r#"mod risc0 {{
-    pub const ELF: &[u8] = include_bytes!({elf_path:?});
-}}"#
-        )
-        .unwrap();
-
         Ok(CompilationResult {
             vm_name: "univm_risc0::Risc0".to_owned(),
             program_name: "univm_risc0::Risc0Program".to_owned(),
             program_impl: format!(
                 r#"pub struct {base_program_name}Risc0(univm_risc0::Risc0Program<$input, $output, $io>);
-                
+
+                impl std::default::Default for {base_program_name}Risc0 {{
+                    fn default() -> Self {{
+                        const ELF: &[u8] = include_bytes!({elf_path:?});
+
+                        Self(univm_risc0::Risc0Program::<$input, $output, $io>::new(ELF))
+                    }}
+                }}
+
                 impl GuestProgram<univm_risc0::Risc0> for {base_program_name}Risc0 {{
                     type Input = $input;
                     type Output = $output;
-                    
-                    fn execute(&self, input: Self::Input) -> Result<(Self::Output, Risc0ExecutionReport), ()> {{
+
+                    fn execute(&self, input: Self::Input) -> Result<(Self::Output, univm_risc0::Risc0ExecutionReport), ()> {{
                         self.0.execute(input);
                     }}
-                    
-                    fn prove(&self, input: Self::Input) -> Result<(Self::Output, Risc0Proof, Risc0ExecutionReport), ()> "#
+
+                    fn prove(&self, input: Self::Input) -> Result<(Self::Output, univm_risc0::Risc0Proof, univm_risc0::Risc0ExecutionReport), ()> {{
+                        self.0.prove(input);
+                    }}
+
+                    fn verify(&self, proof: univm_risc0::Risc0Proof) -> bool {{
+                        self.0.verify(proof);
+                    }}
+                }}"#
             ),
         })
     }
